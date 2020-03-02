6eb55bb9fe6d554ee84c263dde9e8a8c2e1fd2f1
= <=>
2020-03-02 08:11:46 +0100

    [edit] startUrl and version

--- RibsApi.js
+++ RibsApi.js
@@ -5,7 +5,7 @@ class RibsApi {
    * @param credentials of the url like same-origin
    */
   constructor(baseUrl, mode, credentials) {
-    let startUrl = '';
+    let startUrl = `${window.location.protocol}//${window.location.hostname}/`;
 
     if (!window.origin && mode !== 'cors') {
       startUrl = `${window.location.protocol}//${window.location.hostname}${(window.location.port ? `:${window.location.port}` : '')}`;
